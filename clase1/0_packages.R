f6fda0ac397e785961b3c196ba35122988b0edb1
caropradier <carolinapradier@gmail.com>
2023-03-03 19:32:05 -0300

    setup

---
+++ clase1/0_packages.R
@@ -0,0 +1,17 @@
+#Estamos usando la versión 4.2.2 de R
+
+#Estos son los paquetes que vamos a necesitar durante el módulo, recomendamos instalarlos antes de las clases para poder realizar las prácticas
+
+
+install.packages("tidyverse")
+install.packages("viridis")
+install.packages("corrplot")
+install.packages("ggridges")
+install.packages("tidymodels")
+install.packages("GGally")
+install.packages("latex2exp")
+install.packages("car")
+install.packages("gtsummary")
+install.packages("modelsummary")
+install.packages("gt")
+install.packages("discrim")
