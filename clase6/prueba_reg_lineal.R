f1c45a7d4650f14a2805326f5c3b29f83af4f40e
unknown <guidowe45@gmail.com>
2023-04-05 10:55:28 -0300

    clase 6

--- clase6/prueba_reg_lineal.R
+++ clase6/prueba_reg_lineal.R
@@ -46,6 +46,7 @@ lm_spec <- linear_reg() %>%
   # set_mode("regression") %>%
   set_engine("lm")
 
+
 receta <- recipe(x = base_transf,ingreso_laboral ~  precario + grupos.calif + tamanio.establecim + CH04 + CH06 + NIVEL_ED + caes_eph_label+horas_totales + PP03G) %>% 
   step_num2factor(CH04, levels = c("Varon","Mujer")) %>% 
   step_num2factor(PP03G, levels = c("mas_horas","ok_horas")) %>% 
@@ -77,6 +78,71 @@ veo <- tidy(fitted_model) %>% arrange(p.value)
 
 glance(fitted_model)
 
+### Alternativa más parecida a Mincer: 
+## - Solo asalariados, 
+## - log (Ingreso de ocup principal) 
+## - Horas solo de ocup principal
+unique(asalariados$REGION)
+
+asalariados <- base_transf %>% 
+  filter(CAT_OCUP == 3,P21>0,PP3E_TOT>0) %>% # Asalariados con ingreso y con horas
+  mutate(log_salario = log(P21)) %>% 
+  mutate(REGION = factor(REGION,labels = c("GBA","NOA","NEA","Cuyo","Pampeana","Patagonia")))
+
+receta_mincer <- recipe(x = asalariados,log_salario ~  precario + grupos.calif + tamanio.establecim + CH04 + CH06 + NIVEL_ED + caes_eph_label+ PP3E_TOT + REGION) %>% 
+  step_num2factor(CH04, levels = c("Varon","Mujer")) %>% 
+#  step_num2factor(REGION, levels = c("GBA","NOA","NEA","Cuyo","Pampeana","Patagonia")) %>% 
+#  step_num2factor(PP03G, levels = c("mas_horas","ok_horas")) %>% 
+  step_mutate(NIVEL_ED = ifelse(NIVEL_ED == 7,yes = 1,no = NIVEL_ED)) %>% 
+  step_rename(EDAD = CH06,horas_ocup_ppal = PP3E_TOT) %>% 
+  step_num2factor(NIVEL_ED,levels = c("Primaria incompleta","Primaria completa","Secundaria incompleta",
+                                      "Secundaria completa","Terciario/univ incompleta","Terciario/univ completa")) %>% 
+  step_interact( ~ CH04:grupos.calif) %>%
+  step_other(caes_eph_label,threshold = 0.05) 
 
 
+wf <- workflow() %>%
+  add_model(lm_spec) %>% 
+  add_recipe(receta_mincer)  
 
+
+fitted_model_2 <- wf %>% 
+  fit(.,data = asalariados)
+
+ver_model2 <- tidy(fitted_model_2) %>% arrange(p.value)
+
+glance(fitted_model_2)
+
+# Alternativa salario horario #### 
+## - Solo asalariados, 
+## - log (salario horario) 
+## - Horas solo de ocup principal
+
+asalariados_v3 <- base_transf %>% 
+  filter(CAT_OCUP == 3,P21>0,PP3E_TOT>0) %>% # Asalariados con ingreso y con horas
+  mutate(log_salario_horario = log(P21/PP3E_TOT)) %>% 
+  mutate(REGION = factor(REGION,labels = c("GBA","NOA","NEA","Cuyo","Pampeana","Patagonia")))
+
+receta_mincer_v3 <- recipe(x = asalariados_v3,log_salario_horario ~  precario + grupos.calif + tamanio.establecim + CH04 + CH06 + NIVEL_ED + caes_eph_label + REGION) %>% 
+  step_num2factor(CH04, levels = c("Varon","Mujer")) %>% 
+  #  step_num2factor(REGION, levels = c("GBA","NOA","NEA","Cuyo","Pampeana","Patagonia")) %>% 
+  #  step_num2factor(PP03G, levels = c("mas_horas","ok_horas")) %>% 
+  step_mutate(NIVEL_ED = ifelse(NIVEL_ED == 7,yes = 1,no = NIVEL_ED)) %>% 
+  step_rename(EDAD = CH06) %>% 
+  step_num2factor(NIVEL_ED,levels = c("Primaria incompleta","Primaria completa","Secundaria incompleta",
+                                      "Secundaria completa","Terciario/univ incompleta","Terciario/univ completa")) %>% 
+  step_interact( ~ CH04:grupos.calif) %>%
+  step_other(caes_eph_label,threshold = 0.05) 
+
+
+wf3 <- workflow() %>%
+  add_model(lm_spec) %>% 
+  add_recipe(receta_mincer_v3)  
+
+
+fitted_model_3 <- wf3 %>% 
+  fit(.,data = asalariados_v3)
+
+ver_model3 <- tidy(fitted_model_3) %>% arrange(p.value)
+
+glance(fitted_model_3)
